99de52732fba60bd5710446ba0ab364d5f818208
Tanner Stratford <57194030+tannerstratford@users.noreply.github.com>
2021-05-06 23:22:08 -0600

    finished ta03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,45 @@
-//TA03 PLACEHOLDER
+const http = require("https");
 const express = require('express');
 const router = express.Router();
+var stuff = null;
+const test = {
+    myTest: null
+}
+
+function processJson() {
+    var url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+
+    http.get(url, (response) => {
+        var body = '';
+
+        response.on('data', (chunk) => {
+            body += chunk;
+        })
+
+        response.on('end', () => {
+            var jsonResponse = JSON.parse(body);
+
+            test.myTest = jsonResponse;
+
+            //res.render('results', stuff);
+        });
+    }).on('error', (e) => {
+        console.log("Error ", e);
+    });
+}
+
+var data = processJson();
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta03', { 
         title: 'Team Activity 03', 
         path: '/ta03', // For pug, EJS 
         activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+        contentCSS: true,
+        data: test.myTest
     });
 });
 
+
 module.exports = router;
+exports.jsonResponse = { processJson: processJson };
